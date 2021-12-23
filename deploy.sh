70389630f00b02134f1916e9e898628f524711aa
Guillaume Hullin <guillaume.hullin@futur-tech.com>
2021-12-23 21:50:24 +0100

    adding function to getting version installed

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,26 @@ source "$(dirname "$0")/ft-util/ft_util_inc_var"
 
 APP_NAME="futur-tech-zabbix-server"
 
+ZBX_CONF_AGENT_D="/etc/zabbix/zabbix_agentd.conf.d"
+BIN_DIR="/usr/local/bin/${APP_NAME}"
 SRC_DIR="/usr/local/src/${APP_NAME}"
 SUDOERS_ETC="/etc/sudoers.d/${APP_NAME}"
 
 $S_LOG -d $S_NAME "Start $S_DIR_NAME/$S_NAME $*"
+
+echo
+echo "------------------------------------------"
+echo "  INSTALL NEEDED PACKAGES & FILES"
+echo "------------------------------------------"
+echo
+
+ZBX_CONF_AGENT_D="/etc/zabbix/zabbix_agentd.conf.d"
+
+if [ ! -d "${ZBX_CONF_AGENT_D}" ] ; then mkdir "${ZBX_CONF_AGENT_D}" ; $S_LOG -s $? -d $S_NAME "Creating ${ZBX_CONF_AGENT_D} returned EXIT_CODE=$?" ; fi
+if [ ! -d "${BIN_DIR}" ] ; then mkdir "${BIN_DIR}" ; $S_LOG -s $? -d $S_NAME "Creating ${BIN_DIR} returned EXIT_CODE=$?" ; fi
+
+$S_DIR/ft-util/ft_util_file-deploy "$S_DIR/bin/" "${BIN_DIR}"
+$S_DIR/ft-util/ft_util_file-deploy "$S_DIR/etc.zabbix/${APP_NAME}.conf" "${ZBX_CONF_AGENT_D}/${APP_NAME}.conf"
 
 echo
 echo "------------------------------------------"
@@ -21,6 +37,7 @@ $S_LOG -d $S_NAME -d "$SUDOERS_ETC" "==============================="
 
 echo "Defaults:zabbix !requiretty" | sudo EDITOR='tee' visudo --file=$SUDOERS_ETC &>/dev/null
 echo "zabbix ALL=(ALL) NOPASSWD:${SRC_DIR}/deploy-update.sh" | sudo EDITOR='tee -a' visudo --file=$SUDOERS_ETC &>/dev/null
+echo "zabbix ALL=(ALL) NOPASSWD:${BIN_DIR}/zabbix-server-version.sh" | sudo EDITOR='tee -a' visudo --file=$SUDOERS_ETC &>/dev/null
 
 cat $SUDOERS_ETC | $S_LOG -d "$S_NAME" -d "$SUDOERS_ETC" -i 
 
